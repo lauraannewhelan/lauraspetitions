0c5d7c7530caabd36a9c6616119f3a99d354c6cb
Laura Whelan <47507603+lauraannewhelan@users.noreply.github.com>
2024-11-30 15:09:58 +0000

    Adding project from IntelliJ

---
+++ src/main/java/com/laura/spetitions/lauraspetitions/LauraspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.laura.spetitions.lauraspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauraspetitionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LauraspetitionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laura/spetitions/lauraspetitions/LauraspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laura.spetitions.lauraspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauraspetitionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
